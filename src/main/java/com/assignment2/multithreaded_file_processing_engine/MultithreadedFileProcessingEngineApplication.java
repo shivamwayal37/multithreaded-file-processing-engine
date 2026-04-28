3b48acdc385bc08c3ae0ee9dd630e44685933160
Shivam Wayal <wayalshivam7@gmail.com>
2026-04-28 08:18:22 +0000

    chore: bootstrap spring boot project

---
+++ src/main/java/com/assignment2/multithreaded_file_processing_engine/MultithreadedFileProcessingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.assignment2.multithreaded_file_processing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedFileProcessingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedFileProcessingEngineApplication.class, args);
+	}
+
+}
